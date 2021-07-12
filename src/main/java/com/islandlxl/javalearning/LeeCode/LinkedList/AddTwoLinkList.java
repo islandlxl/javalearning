6b6366142c7c511887b14ca33e8221b6001c6876
islandlxl <569881564@qq.com>
2021-07-12 23:27:55 +0800

    两个链表数字之和

--- src/main/java/com/islandlxl/javalearning/LeeCode/LinkedList/AddTwoLinkList.java
+++ src/main/java/com/islandlxl/javalearning/LeeCode/LinkedList/AddTwoLinkList.java
@@ -1,5 +1,8 @@
 package com.islandlxl.javalearning.LeeCode.LinkedList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 @project:com.LeeCode.LinkedList
 @Title:AddTwoLinkList
@@ -10,8 +13,28 @@ package com.islandlxl.javalearning.LeeCode.LinkedList;
 
     public static void main(String[] args) {
 
+        ListNode p1 = new ListNode(2);
+        ListNode p2 = new ListNode(4);
+        ListNode p3 = new ListNode(3);
 
+        p1.next=p2;
+        p2.next=p3;
+
+        ListNode q1 = new ListNode(5);
+        ListNode q2 = new ListNode(6);
+        ListNode q3 = new ListNode(4);
+
+        q1.next=q2;
+        q2.next=q3;
+
+        ListNode listNode = addTwoNumbers(p1, q1);
+        ListNode curr=listNode;
+        while (curr!=null){
+            System.out.print("curr = " + curr.val);
+            curr=curr.next;
+        }
     }
+    //此答案错误
     public static ListNode solution(ListNode l1, ListNode l2){
         ListNode l3 = new ListNode(0);
         ListNode prev = l3;
@@ -36,6 +59,65 @@ package com.islandlxl.javalearning.LeeCode.LinkedList;
             this.val = x;
         }
     }
+
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        int floatNum=0;
+        int sumNum=0;
+        int  nextVal=0;
+        List<Integer> list=new ArrayList<>();
+        while (l1!=null||l2!=null){
+            if (l1!=null&&l2!=null){
+                sumNum= l1.val+l2.val+floatNum;
+                l1=l1.next;
+                l2=l2.next;
+            }else if (l1!=null&&l2==null){
+                sumNum=l1.val+floatNum;
+                l1=l1.next;
+            }else if (l1==null&&l2!=null){
+               sumNum =l2.val+floatNum;
+                l2=l2.next;
+            }
+            nextVal=sumNum%10;
+            floatNum=sumNum/10;
+            list.add(nextVal);
+        }
+
+        if (floatNum>0){
+            list.add(1);
+        }
+        ListNode head=new ListNode(0);
+        ListNode cur=head;
+        for (Integer val:list){
+            ListNode curr=new ListNode(val);
+            cur.next=curr;
+            cur=cur.next;
+        }
+        return head.next;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
